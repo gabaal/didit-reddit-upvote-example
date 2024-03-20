630c190367d77bdd528ecc193ed9b8be1497d199
gabaal <geraldbaalham@live.co.uk>
2024-03-20 14:44:01 +0000

    tidied up global error message

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -9,7 +9,7 @@ export default function GlobalError({ error, reset }) {
         <h2 className="text-3xl">Oh no! Something went wrong on that page!</h2>
         <p className="max-w-screen-lg mx-auto p-4 mb-4">{error.message}</p>
         {/* <button className="max-w-screen-lg mx-auto p-4 mb-4"onClick={() => reset()}>Try again</button> */}
-        <Link className="max-w-screen-lg mx-auto p-4 mb-4" href="/">Return to homepage </Link>
+        {/* <Link className="max-w-screen-lg mx-auto p-4 mb-4" href="/">Return to homepage </Link> */}
       </body>
     </html>
   );
